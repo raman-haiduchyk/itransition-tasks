8d280fc603b00dca71f6efa969997507b3d1c30c
haid_roman <haidroman@gmail.com>
2021-03-28 19:32:10 +0300

    added profile functionality

--- main-task/webapi/Controllers/ProfileController.cs
+++ main-task/webapi/Controllers/ProfileController.cs
@@ -29,10 +29,11 @@ namespace webapi.Controllers
             _userManager = userManager;
         }
 
-        [HttpGet]
+        [HttpGet("myprofile")]
         public async Task<IActionResult> GetProfile()
         {
             var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (user == null) return StatusCode(404, "No such user");
             return StatusCode(200, new ProfileResponseModel()
             {
                 Name = user.UserName,
@@ -42,6 +43,44 @@ namespace webapi.Controllers
                 Phome = user.PhoneNumber,
                 Role = await _userManager.IsInRoleAsync(user, "admin") ? "admin" : "user"
             });
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpPost("profilebyid")]
+        public async Task<IActionResult> GetProfileById(ProfileByIdRequestModel requestModel)
+        {
+            var user = await _userManager.FindByIdAsync(requestModel.Id);
+            if (user == null) return StatusCode(404, "No such user");
+            return StatusCode(200, new ProfileResponseModel()
+            {
+                Name = user.UserName,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Phome = user.PhoneNumber,
+                Role = await _userManager.IsInRoleAsync(user, "admin") ? "admin" : "user"
+            });
+        }
+
+        [HttpPost("changeprofile")]
+        public async Task<IActionResult> ChangeProfile(ProfileResponseModel requestModel)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (user == null) return StatusCode(404, "No such user");
+            user.FirstName = requestModel.FirstName;
+            user.LastName = requestModel.LastName;
+            user.PhoneNumber = requestModel.Phome;
+            user.Email = requestModel.Email;
+            try
+            {
+                await _userManager.UpdateAsync(user);
+            }
+            catch
+            {
+                return StatusCode(409);
+            }
+
+            return StatusCode(200);
         }
     }
 }
